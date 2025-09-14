2ffb6fb51538bfe89111ac1444ea12038ee972c7
Michael Pardo <mpardo@netflix.com>
2025-09-13 22:33:49 -0400

    Initial configuration.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Disable terminal toggle
+vim.keymap.del({ "n" }, "<c-/>")
+vim.keymap.del({ "t" }, "<C-/>")
+vim.keymap.del({ "n" }, "<c-_>")
+vim.keymap.del({ "t" }, "<C-_>")
